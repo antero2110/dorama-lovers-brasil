292bfdfddd14dcbc3c8e7f2981d2bef6b62a9782
antero2110 <anterofl10@gmail.com>
2026-06-07 05:34:28 -0300

    debug url

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,21 @@
 import { supabase } from "@/src/lib/supabase";
-import BuscaDoramas from "./components/BuscaDoramas";
 
 export default async function Home() {
-  const { data: doramas, error } = await supabase
+  const { data, error } = await supabase
     .from("doramas")
     .select("*");
 
   return (
-    <main>
-      <pre>
-        {JSON.stringify(
-          {
-            quantidade: doramas?.length,
-            doramas,
-            error,
-          },
-          null,
-          2
-        )}
-      </pre>
-    </main>
+    <pre>
+      {JSON.stringify(
+        {
+          url: process.env.NEXT_PUBLIC_SUPABASE_URL,
+          data,
+          error,
+        },
+        null,
+        2
+      )}
+    </pre>
   );
 }
