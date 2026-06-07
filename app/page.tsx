1056e974b6ca5806a352df1e90f96bacd9ea1a15
antero2110 <anterofl10@gmail.com>
2026-06-07 05:25:27 -0300

    teste dados

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
+import { supabase } from "@/src/lib/supabase";
+import BuscaDoramas from "./components/BuscaDoramas";
+
 export default async function Home() {
   const { data: doramas, error } = await supabase
     .from("doramas")
     .select("*");
 
-  console.log("DORAMAS:", doramas);
-  console.log("ERRO:", error);
-
   return (
     <main>
       <pre>
-        {JSON.stringify({ doramas, error }, null, 2)}
+        {JSON.stringify(
+          {
+            quantidade: doramas?.length,
+            doramas,
+            error,
+          },
+          null,
+          2
+        )}
       </pre>
     </main>
   );
