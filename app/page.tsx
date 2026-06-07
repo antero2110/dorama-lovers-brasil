47eb4ef95ddd28326e86d86bf3251ac48386d469
antero2110 <anterofl10@gmail.com>
2026-06-07 05:23:40 -0300

    teste supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,16 @@
-import { supabase } from "@/src/lib/supabase";
-import BuscaDoramas from "./components/BuscaDoramas";
-
 export default async function Home() {
-  const { data: doramas } = await supabase
+  const { data: doramas, error } = await supabase
     .from("doramas")
-    .select("*")
-    .order("id");
+    .select("*");
+
+  console.log("DORAMAS:", doramas);
+  console.log("ERRO:", error);
 
   return (
-    <main className="min-h-screen bg-pink-50 p-8">
-      <div className="max-w-5xl mx-auto">
-        <h1 className="text-5xl font-bold text-center text-pink-600">
-          💖 Dorama Lovers Brasil
-        </h1>
-
-        <p className="text-center text-gray-800 mt-4 text-xl">
-          Descubra, avalie e acompanhe seus doramas favoritos
-        </p>
-
-        <BuscaDoramas doramas={doramas || []} />
-      </div>
+    <main>
+      <pre>
+        {JSON.stringify({ doramas, error }, null, 2)}
+      </pre>
     </main>
   );
 }
