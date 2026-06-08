033559e70a43b11648ff88e4d4fe25abd67b79ae
antero2110 <anterofl10@gmail.com>
2026-06-08 08:05:08 -0300

    corrige cores dos textos

--- app/page.tsx
+++ app/page.tsx
@@ -27,16 +27,24 @@ export default async function Home() {
   className="w-full p-4 rounded-2xl border-2 border-pink-200 shadow-md focus:outline-none focus:border-pink-500 text-gray-800 placeholder:text-gray-400"
  />
       </div>
+<div className="max-w-7xl mx-auto px-4 mb-8">
+  <h2 className="text-3xl font-bold text-pink-600">
+    🌸 Doramas Cadastrados
+  </h2>
 
+  <p className="text-gray-500 mt-2">
+    Confira as avaliações da Ana e descubra novos doramas.
+  </p>
+</div>
       {/* Cards */}
-      <div className="max-w-7xl mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 justify-items-center">
+      <div className="max-w-7xl mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 place-items-center">
 {doramas?.map((dorama) => (
   <Link
     key={dorama.id}
     href={`/dorama/${dorama.id}`}
     className="block"
   >
-    <div className="bg-white rounded-3xl shadow-xl overflow-hidden hover:scale-105 hover:-translate-y-2 hover:shadow-2xl transition-all duration-300 w-72 cursor-pointer">
+    <div className="bg-white rounded-3xl shadow-xl overflow-hidden hover:scale-105 hover:-translate-y-3 hover:shadow-pink-300 hover:shadow-2xl transition-all duration-300 w-72 cursor-pointer border border-pink-100">
             <img
               src={dorama.capa_url}
               alt={dorama.titulo}
@@ -47,7 +55,7 @@ export default async function Home() {
               <h2 className="font-bold text-xl text-gray-800 line-clamp-2">
                 {dorama.titulo}
               </h2>
-			  <p className="text-gray-500 text-sm mt-2 line-clamp-3">
+<p className="text-gray-500 text-sm mt-2 h-16 overflow-hidden">
   {dorama.sinopse}
 </p>
 
@@ -57,9 +65,11 @@ export default async function Home() {
                 </span>
               </div>
 
-              <p className="text-pink-500 font-medium mt-2">
-                💖 {dorama.genero}
-              </p>
+<div className="mt-2">
+  <span className="bg-pink-100 text-pink-600 px-3 py-1 rounded-full text-sm font-semibold">
+    {dorama.genero}
+  </span>
+</div>
 
               <p className="text-gray-500 text-sm mt-3">
                 📅 {dorama.ano}
